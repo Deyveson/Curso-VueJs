59ee62fac2801a291d3cda7c88d49a15cd686585
deyveson <deyvesonn@outlook.com>
2019-07-01 13:27:28 -0400

    subindo desafio

--- dom/desafio-01/app.js
+++ dom/desafio-01/app.js
@@ -1,14 +1,16 @@
 new Vue({
-    el: "#desafio",
-    data:{
-        nome: "Deyveson Willian",
-        idade: "25",
-        link: 'https://i.imgur.com/A87AW0Z.jpg'
+  el: "#desafio",
+  data: {
+    nome: "Deyveson Willian",
+    idade: 25,
+    imagem: "http://files.cod3r.com.br/curso-vue/vue.jpg"
+  },
+  methods: {
+    random: function() {
+      return Math.random();
     },
-    methods: {
-        randomico: function() {
-          
-          return Math.random();
-        }
-      }
-})
+    idadeVezes3(){
+        return this.idade * 3;
+    }
+  }
+});
